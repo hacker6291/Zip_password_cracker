11fb7dd235b83e7d151c845ec83e33a046321b2d
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-03-09 10:59:54 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,18 @@
+echo -e "\e[1;31m"
+figlet Zip Password cracker
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border QADIR
+
+echo " "
+apt update
+apt upgrade
+pkg install python
+pkg install python3
+pkg install git
+git clone https://github.com/hacker6291/Zip_password_cracker.git
+pip install zipfile
+pip install os
+pip install colored
+cd Zip_password_cracker
+python3 Cracker.py
+                 
